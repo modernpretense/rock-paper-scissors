6b55c44bd35160b26a4276be526ab52d94bfb7be
Morten <7154135+modernpretense@users.noreply.github.com>
2023-02-15 19:32:33 +0100

    function getComputerChoice up and running

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Rock-Paper-Scissors!
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 0) {
+        return 'Rock';
+    } else if (choice === 1) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }   
+}
+
+console.log(getComputerChoice());
